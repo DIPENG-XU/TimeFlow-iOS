4803d958a4f214c0143770da9edc48ef7016c9c3
apollo <dipengxu@163.com>
2024-06-30 13:41:23 +0800

    Finish the font in iPad

--- TimeFlow/Utils/Utils.swift
+++ TimeFlow/Utils/Utils.swift
@@ -3,6 +3,6 @@ import UIKit
 
 let isPad: Bool = (UIDevice.current.userInterfaceIdiom == .pad)
 
-let timeFont: Double = isPad ? 216.0 : 144.0
+let timeFont: Double = isPad ? 288.0 : 144.0
 
 let dateFont: Double = 32.0
